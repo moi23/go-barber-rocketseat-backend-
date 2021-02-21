1b11ef35c841e669707bc7827da1db1ca573c38f
moi23 <moisesboas@gmail.com>
2021-02-21 04:44:52 -0300

    feat: models appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,27 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import Appointment from '../models/Appointments';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
-
+const appointments: Appointment[] = [];
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
-  appointments.push(appointment);
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find((i) =>
+    isEqual(parsedDate, i.date)
+  );
 
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = new Appointment(provider, parsedDate);
+
+  appointments.push(appointment);
   return response.json(appointment);
 });
 
